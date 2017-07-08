35960bb8be8621d30688ab25fb1de7b154ed7db4
Venus Kuo <vkuo126@gmail.com>
2017-07-08 19:47:03 -0400

    Maximum liklihood tree construct

---
+++ phylo/Brassica.ML.1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -k o
+#PBS -l nodes=2:ppn=8,vmem=100gb,walltime=5:00:00
+#PBS -M lennonj@indiana.edu
+#PBS -m abe
+#PBS -j oe
+
+module load python/2.7.3
+module load biopython/1.63
+module load fasttree/2.1.1
+
+cd /N/dc2/projects/Lennon_Sequences/2017BrassicaRpf
+
+python name_change.py "Brassica.bac.final.0.03.rep.fasta" "Brassica.bac.final.0.03.rep.rename.fasta"
+
+fasttree -gtr -nt -gamma -fastest Brassica.bac.final.0.03.rep.rename.fasta > Brassica.bac.rename.tree.1
